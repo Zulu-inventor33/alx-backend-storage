68a9cfb97b0aa4f0808a5d2bc1b7e0d93c22a7a3
Zulu-inventor33 <muteakelvin15@gmail.com>
2024-10-17 13:25:31 +0000

    question one

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Script to create a 'users' table with constraints
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
